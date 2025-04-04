a11591e12ee09dcc6f256871b96b3fd4e7e7e793
ishaqznz <ishaquelaman@gmail.com>
2025-04-04 14:11:25 +0530

    Add views folder with index.ejs and fix path import

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const path = require('path')
 const userRouter = require('./routes/userRouter')
 const { connectDB } = require('./config/connectDB')
 const session = require('express-session');
@@ -17,16 +18,19 @@ app.use(
 );
 
 
-app.use('/', userRouter)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
 app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "views"));
+
+app.use('/', userRouter)
 
 
 
 //Connecting to the database
 connectDB()
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}!`);
